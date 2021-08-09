2c7a1e50dab3e1906332c54c937bd3c51155ec3e
DvijIt <andreydnb@ukr.net>
2021-08-09 22:44:31 +0300

    lesson5 ex

--- src/main/java/lesson5/Demo.java
+++ src/main/java/lesson5/Demo.java
@@ -1,21 +1,10 @@
 package lesson5;
 
-import org.hibernate.Session;
 
 public class Demo {
     public static void main(String[] args) {
-        Session session = new HibernateUtils().createSessionFactory().openSession();
-
-        session.getTransaction().begin();
-
         Product product = new Product(11234, "TestNameAgain", "Test ewfeafDescription", 1017);
 
-        session.save(product);
 
-        session.getTransaction().commit();
-
-        System.out.println("Done");
-
-        session.close();
     }
 }
